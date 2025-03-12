a7ede85e110ca547a6ea474ef25b6c732fd3241a
Henrique Corrales Marchiori <85183717+HMarchiori@users.noreply.github.com>
2025-03-12 00:43:12 +0000

    Alteracao 1 no main

--- projeto1b/src/App.java
+++ projeto1b/src/App.java
@@ -10,5 +10,7 @@ public class App {
         System.out.println(p.toString());
         System.out.println("Categoria: "+p.classifica());
         s.close();  
+
+        //comentario escrito na branch main
     }
 }
